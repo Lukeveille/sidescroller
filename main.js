f79a7604af8ede6a68e9eba6782fefbbddef301c
Lukeveille <luke@lukeleveille.com>
2020-05-18 10:49:02 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', e => {
+  
+  'use strict';
+
+  const render = () => {
+    display.renderColor(game.color);
+    display.render();
+  }
+
+  const update = () => {
+    game.update();
+  }
+  
+  const controller = new Controller();
+  
+  const display = new Display(document.querySelector('canvas'));
+  
+  const game = new Game();
+  
+  const engine = new Engine(1000/30, render, update);
+  
+  window.addEventListener('resize', display.handleResize);
+  window.addEventListener('keydown', controller.handleKeyDownUp);
+  window.addEventListener('keyup', controller.handleKeyDownUp);
+
+  display.resize();
+  engine.start();
+})
